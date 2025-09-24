85716d29bb080b3341a8db43115dd301fd88f50a
MarcelloZAZ <168892928+MarcelloZAZ@users.noreply.github.com>
2025-09-24 14:18:50 +0700

    Commit No. 3 - Load and Display Cat Image using Glide

--- app/src/main/java/com/example/lab_week_05/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_05/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.lab_week_05
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
 import com.example.lab_week_05.api.CatApiService
 import com.example.lab_week_05.model.ImageData
@@ -18,12 +19,20 @@ class MainActivity : AppCompatActivity() {
             .build()
     }
 
-    // Cat API service
     private val catApiService by lazy {
         retrofit.create(CatApiService::class.java)
     }
+
     private val apiResponseView: TextView by lazy {
         findViewById(R.id.api_response)
+    }
+
+    private val imageResultView: ImageView by lazy {
+        findViewById(R.id.image_result)
+    }
+
+    private val imageLoader: ImageLoader by lazy {
+        GlideLoader(this)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,7 +44,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun getCatImageResponse() {
         val call = catApiService.searchImages(1, "full")
-
         call.enqueue(object : Callback<List<ImageData>> {
             override fun onFailure(call: Call<List<ImageData>>, t: Throwable) {
                 Log.e(MAIN_ACTIVITY, "Failed to get response", t)
@@ -47,7 +55,14 @@ class MainActivity : AppCompatActivity() {
             ) {
                 if (response.isSuccessful) {
                     val image = response.body()
-                    val firstImage = image?.firstOrNull()?.imageUrl ?: "No URL"
+                    val firstImage = image?.firstOrNull()?.imageUrl.orEmpty()
+
+                    if (firstImage.isNotBlank()) {
+                        imageLoader.loadImage(firstImage, imageResultView)
+                    } else {
+                        Log.d(MAIN_ACTIVITY, "Missing image URL")
+                    }
+
                     apiResponseView.text = getString(R.string.image_placeholder, firstImage)
                 } else {
                     Log.e(
